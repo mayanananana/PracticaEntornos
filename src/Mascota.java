c912a0407b152eccd1cfb60d203b63efbc606214
Mariana <marianagomezdam@gmail.com>
2025-05-01 16:19:51 +0200

    Version inicial

---
+++ src/Mascota.java
@@ -0,0 +1,119 @@
+import java.util.Objects;
+
+public class Mascota {
+    private int id;
+    private String nombre;
+    private int edad;
+    private String especie;
+    private boolean vacunado;
+    private int visitas;
+    private static int count=1;
+
+
+    public Mascota() {
+        this.id = count;
+        count++;
+    }
+
+    public Mascota(String nombre, int edad, String especie, boolean vacunado) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.especie = especie;
+        this.vacunado = vacunado;
+        this.visitas=0;
+        this.id= count; count++;
+
+    }
+
+    public Mascota(String nombre, int edad, String especie, boolean vacunado, int visitasUser) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.especie = especie;
+        this.vacunado = vacunado;
+        this.visitas=visitasUser;
+        this.id= count; count++;
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public boolean isVacunado() {
+        return vacunado;
+    }
+
+    public void setVacunado(boolean vacunado) {
+        this.vacunado = vacunado;
+    }
+
+    public int getVisitas() {
+        return visitas;
+    }
+
+    public void setVisitas(int visitas) {
+        this.visitas = visitas;
+    }
+
+    public void vacunar() {
+        this.vacunado = true;
+    }
+
+    public void registrarVisita() {
+        this.visitas++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Mascota{" +
+                "nombre='" + nombre + '\'' +
+                ", edad=" + edad +
+                ", especie='" + especie + '\'' +
+                ", vacunado=" + vacunado +
+                ", visitas=" + visitas +
+                ", id=" + id +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Mascota mascota = (Mascota) o;
+        return id == mascota.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
+
---
+++ src/Veterinaria.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class Veterinaria {
+    private ArrayList<Mascota> mascotas;
+
+    public Veterinaria() {
+    }
+
+    public Veterinaria(ArrayList<Mascota> mascotas) {
+        this.mascotas = mascotas;
+    }
+
+    public ArrayList<Mascota> getMascotas() {
+        return mascotas;
+    }
+
+    public void setMascotas(ArrayList<Mascota> mascotas) {
+        this.mascotas = mascotas;
+    }
+
+    public void agregarMascota(Mascota m) {
+        mascotas.add(m);
+    }
+
+
+    @Override
+    public String toString() {
+        return "Veterinaria{" +
+                ", mascotas=" + mascotas +
+                '}';
+    }
+}
+
