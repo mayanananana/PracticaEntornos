353004b88744a79e9506a35be349fea11633614e
mayanananana <marianagomezdam@gmail.com>
2025-05-02 13:33:03 +0200

    Inline de logica de mostrar mascotas

--- src/Main.java
+++ src/Main.java
@@ -33,7 +33,9 @@ public class Main {
                     break;
                 case 4:
                     // Lógica para mostrar mascotas
-                    mostrarMascotas();
+                    for (Mascota m: mascotas) {
+                        System.out.println(m);
+                    }        
                     break;
                 case 5:
                     // Lógica para mostrar estadísticas generales
@@ -65,11 +67,7 @@ public class Main {
         System.out.println("Total visitas: " + totalVisitas);
     }
 
-    private static void mostrarMascotas() {
-        for (Mascota m: mascotas) {
-            System.out.println(m);
-        }
-    }
+
 
     private static void vacunarMascota(Scanner sc) {
         Mascota mascotaVacunar= new Mascota();
